05a921521e65fe3b47d51346a56cf2da7a9b862f
Eder Sampaio <eder.sampaio@gold360.com.br>
2018-09-21 12:58:14 -0300

    [FEAT] refactor gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,257 +1,176 @@
-'use strict';
+'use strict'
 
-let gulp 					= require('gulp'),
-		browserSync 	= require('browser-sync'),
-		plumber  			= require('gulp-plumber'),
-		logger 				= require('gulp-logger'),
-		sass 					= require('gulp-sass'),
-		concat 				= require('gulp-concat'),
-		debug 				= require('gulp-strip-debug'),
-		uglify 				= require('gulp-uglify'),
-		jshint 				= require('gulp-jshint'),
-		jshintStylish = require('jshint-stylish'),
-		imagemin			= require('gulp-imagemin'),
-		spritesmith 	= require('gulp.spritesmith'),
-		svgSprite 		= require('gulp-svg-sprites'),
-		usemin 				= require('gulp-usemin'),
-		htmlmin 			= require('gulp-htmlmin'),
-		cssmin 				= require('gulp-cssmin'),
-		rev 					= require('gulp-rev'),
-		clean 				= require('gulp-clean');
+const gulp = require('gulp')
+const browserSync = require('browser-sync')
+const plumber = require('gulp-plumber')
+const logger = require('gulp-logger')
+const sass = require('gulp-sass')
+const concat = require('gulp-concat')
+const debug = require('gulp-strip-debug')
+const uglify = require('gulp-uglify')
+const jshint = require('gulp-jshint')
+const jshintStylish = require('jshint-stylish')
+const imagemin = require('gulp-imagemin')
+const spritesmith = require('gulp.spritesmith')
+const svgSprite = require('gulp-svg-sprites')
+const usemin = require('gulp-usemin')
+const htmlmin = require('gulp-htmlmin')
+const cssmin = require('gulp-cssmin')
+const rev = require('gulp-rev')
+const clean = require('gulp-clean')
 
-
-var onError = function (error) {
-  console.log(error.message);
-  this.emit('end');
+const onError = (err) => {
+  console.log(err.message)
+  this.emit('end')
 }
 
+const paths = {
+  src: 'src',
+  dist: 'app'
+}
 
-/**
- *	Compiles sass files (asynchronous task)
- *	[https://www.npmjs.com/package/gulp-sass]
- */
+gulp.task('sass', () => (
+  gulp.src(`${paths.src}/scss/**/*.scss`)
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(logger({
+      before: 'starting sass task...',
+      after: 'sass task complete!',
+      extname: '.scss',
+      showChange: true
+    }))
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(gulp.dest(`${paths.dist}/assets/css/`))
+))
 
-gulp.task('sass', () => {
+gulp.task('jshint', () => (
+  gulp.src(`${paths.src}/js/**/*.js`)
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(logger({
+      before: 'starting hint task...',
+      after: 'hint task complete!',
+      showChange: true
+    }))
+    .pipe(jshint())
+    .pipe(jshint.reporter(jshintStylish))
+))
 
-	gulp.src('src/scss/**/*.scss')
-		.pipe(plumber({ errorHandler: onError }))
-		.pipe(logger({
-			before: 'starting sass task...',
-			after: 'sass task complete!',
-			extname: '.scss',
-			showChange: true
-		}))
-		.pipe(sass({ outputStyle: 'compressed' }))
-		.pipe(gulp.dest('app/assets/css/'))
-});
-
-
-/**
- *	Detects errors and potential problems in JavaScript code (synchronous task)
- *	[https://www.npmjs.com/package/gulp-jshint]
- */
-
-gulp.task('jshint', () =>
-
-	gulp.src('src/js/**/*.js')
-		.pipe(plumber({ errorHandler: onError }))
-		.pipe(logger({
-			before: 'starting hint task...',
-			after: 'hint task complete!',
-			showChange: true
-		}))
-		.pipe(jshint())
-		.pipe(jshint.reporter(jshintStylish))
-);
-
-
-/**
- *	Concatenates, removes debugs (cosole.log(), alert(), ...) and minifies JavaScript (asynchronous task)
- *	gulp-concat 		 => [https://www.npmjs.com/package/gulp-concat]
- *	gulp-strip-debug => [https://www.npmjs.com/package/gulp-strip-debug]
- * 	gulp-uglify 		 => [https://www.npmjs.com/package/gulp-uglify]
- */
-
-gulp.task('scripts', ['jshint'], () => {
-
+gulp.task('scripts', ['jshint'], () => (
   gulp.src([
-    'src/js/jquerytojs.min.js'
+    `${paths.src}/js/func1.js`,
+    `${paths.src}/js/func2.js`
   ])
-  	.pipe(plumber({ errorHandler: onError }))
-		.pipe(logger({
-			before: 'starting scripts task...',
-			after: 'scripts task complete!',
-			extname: '.js',
-			showChange: true
-		}))
-	  .pipe(concat('app.min.js'))
-	  .pipe(debug())
-	  .pipe(uglify())
-	  .pipe(gulp.dest('app/assets/js/'));
-});
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(logger({
+      before: 'starting scripts task...',
+      after: 'scripts task complete!',
+      extname: '.js',
+      showChange: true
+    }))
+    .pipe(concat('app.min.js'))
+    .pipe(debug())
+    .pipe(uglify())
+    .pipe(gulp.dest(`${paths.dist}/assets/js/`))
+))
 
+gulp.task('images', () => (
+  gulp.src(`${paths.src}/**/*.{jpg,png,svg}`)
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(logger({
+      before: 'starting images task...',
+      after: 'images task complete!',
+      showChange: true
+    }))
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.optipng({ optimizationLevel: 7 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true }
+        ]
+      })
+    ]))
+    .pipe(gulp.dest(`${paths.dist}/assets/`))
+))
 
-/**
- *	Optimize images (asynchronous task)
- *	gulp-imagemin => [https://www.npmjs.com/package/gulp-imagemin]
- */
+gulp.task('svg-sprite', ['images'], () => (
+  gulp.src(`${paths.dist}/assets/svg/icons/*.svg`)
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(logger({
+      before: 'starting svg-sprite task...',
+      after: 'svg-sprite task complete!',
+      showChange: true
+    }))
+    .pipe(svgSprite({
+      selector: 'icon-svg-%f',
+      svg: { sprite: 'sprite/sprite-svg.svg' },
+      preview: { sprite: 'svg-sprite-preview.html' },
+      templates: { scss: true }
+    }))
+    .pipe(gulp.dest(`${paths.dist}/assets/`))
+))
 
-gulp.task('images', () =>
+gulp.task('sprite', () => (
+  gulp.src(`${paths.src}/img/sprite-icons/**/*.png`)
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(logger({
+      before: 'starting sprite task...',
+      after: 'sprite task complete!',
+      showChange: true
+    }))
+    .pipe(spritesmith({
+      imgName: 'sprite.png',
+      // cssName: '_sprite.scss'
+      cssName: '_sprite.css'
+    }))
+    .pipe(gulp.dest(`${paths.dist}/assets/img/sprites`))
+))
 
-  gulp.src('src/**/*.{jpg,png,svg}')
-		.pipe(plumber({ errorHandler: onError }))
-		.pipe(logger({
-			before: 'starting images task...',
-			after: 'images task complete!',
-			showChange: true
-		}))
-		.pipe(imagemin([
-		  // imagemin.gifsicle({interlaced: true}),
-		  // imagemin.jpegtran({progressive: true}),
-		  // imagemin.optipng({optimizationLevel: 7}),
-		  // imagemin.svgo({plugins: [{removeViewBox: true}]})
-		  interlaced: true,
-		  progressive: true,
-		  optimizationLevel: 7,
-		  svgoPlugins: [{removeViewBox: true}]
-		]))
-    .pipe(gulp.dest('app/assets/'))
-);
-
-
-/**
- *	Generated SVG sprite
- *	[https://www.npmjs.com/package/gulp-svg-sprites]
- */
-
-gulp.task('svg-sprite', ['images'], () => {
-
-	gulp.src('app/assets/svg/icons/*.svg')
-		.pipe(plumber({ errorHandler: onError }))
-		.pipe(logger({
-			before: 'starting svg-sprite task...',
-			after: 'svg-sprite task complete!',
-			showChange: true
-		}))
-		.pipe(svgSprite({
-			selector: "icon-svg-%f",
-			svg: { sprite: "sprite/sprite-svg.svg" },
-			preview: { sprite: "preview.html" },
-			templates: { scss: true }
-		}))
-		.pipe(gulp.dest('app/assets/'))
-});
-
-
-/**
- *	Convert a set of images into a spritesheet and CSS variables (asynchronous task)
- *	gulp.spritesmith => [https://www.npmjs.com/package/gulp.spritesmith]
- */
-
-gulp.task('sprite', () => {
-
-  gulp.src('src/img/sprite-icons/**/*.png')
-		.pipe(plumber({ errorHandler: onError }))
-		.pipe(logger({
-			before: 'starting sprite task...',
-			after: 'sprite task complete!',
-			showChange: true
-		}))
-	  .pipe(spritesmith({
-	    imgName: 'sprite.png',
-	    // cssName: '_sprite.scss'
-	    cssName: '_sprite.css'
-	  }))
-  	.pipe(gulp.dest('app/assets/img/sprites'));
-});
-
-
-/**
- *	Replaces references to non-optimized scripts or stylesheets
- *	into a set of HTML files (or any templates/views) (asynchronous task)
- *	gulp-usemin => [https://www.npmjs.com/package/gulp-usemin]
- */
-
-gulp.task('usemin', () => {
-
-  gulp.src('src/html/**/*.html')
-		.pipe(plumber({ errorHandler: onError }))
-		.pipe(logger({
-			before: 'starting usemin task...',
-			after: 'usemin task complete!',
-			showChange: true
-		}))
+gulp.task('usemin', () => (
+  gulp.src(`${paths.src}/html/**/*.html`)
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(logger({
+      before: 'starting usemin task...',
+      after: 'usemin task complete!',
+      showChange: true
+    }))
     .pipe(usemin({
       css: [ cssmin, rev ],
       html: [
-      	htmlmin({
-      		collapseWhitespace: true,
-      		removeComments: true,
-      		minifyCSS: true,
-      		minifyJS: true
-      	})
+        htmlmin({
+          collapseWhitespace: true,
+          removeComments: true,
+          minifyCSS: true,
+          minifyJS: true
+        })
       ],
       js: [ uglify, rev ],
       inlinejs: [ uglify ]
     }))
-    .pipe(gulp.dest('app/'));
-});
-
-
-/**
- *	Checks changes to .js, .scss, and .html files
- *	and run related tasks (asynchronous task)
- */
+    .pipe(gulp.dest(paths.dist))
+))
 
 gulp.task('watch', () => {
+  gulp.watch(`${paths.src}/js/**/*.js`, ['scripts'])
+  gulp.watch(`${paths.src}/scss/**/*.scss`, ['sass'])
+  gulp.watch(`${paths.src}/html/**/*.html`, ['htmlmin'])
+})
 
-	gulp.watch('src/js/**/*.js', ['scripts']);
-	gulp.watch('src/scss/**/*.scss', ['sass']);
-	gulp.watch('src/html/**/*.html', ['htmlmin']);
-});
+gulp.task('clean', () => (
+  gulp.src(`${paths.dist}/assets/**/*`, { read: false })
+    .pipe(clean({ force: true }))
+))
 
+gulp.task('build', ['clean'], () => (
+  gulp.start('sass', 'scripts', 'usemin')
+))
 
-/**
- *	Removes folders and files from the app folder (synchronous task)
- *	gulp-clean => [https://www.npmjs.com/package/gulp-clean]
- */
+gulp.task('server', ['watch', 'build'], () => {
+  browserSync.init({
+    server: { baseDir: paths.dist }
+  })
 
-gulp.task('clean', () =>
+  gulp.watch(`${paths.dist}/**/*`).on('change', browserSync.reload)
+})
 
-	gulp.src('app/**/*', { read: false })
-		.pipe(clean({ force: true }))
-);
-
-
-/**
- *	Runs all tasks (synchronous task)
- */
-
-gulp.task('build', ['clean'], () =>
-
-	gulp.start('sass', 'scripts', 'usemin', 'watch')
-);
-
-
-/**
- *	Keep multiple browsers & devices in sync when building websites (asynchronous task)
- *	browser-sync => [https://www.npmjs.com/package/browser-sync]
- */
-
-gulp.task('server', ['build'], () => {
-
-	browserSync.init({
-		server: {
-			baseDir: 'app/'
-		}
-	});
-
-	gulp.watch('src/**/*').on('change', browserSync.reload);
-});
-
-
-/**
- *	Runs default task
- */
-
-gulp.task('default', ['server']);
+gulp.task('default', ['server'])
